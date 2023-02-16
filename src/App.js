5cb420e72710da15d7fbdea4aa49e8bd2dc41836
OlgaOrya <olga_b@bk.ru>
2023-02-16 20:51:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
+import picture from './picture.jpg'
 
 function App() {
+
+  const [activities, setActivities] = useState ('');
+  const [participantsActivity,setParticipantsActivity] = useState('');
+  const [typeActivity,setTypeActivity] = useState('')
+
+useEffect (() =>{
+  showAdvice()
+}, [])
+
+const showAdvice = useCallback (async() =>{
+  const response = await fetch (`http://www.boredapi.com/api/activity/`);
+  const data = await response.json();
+  console.log (data)
+  setActivities(data.activity)
+  setParticipantsActivity (data.participants)
+  setTypeActivity(data.type)
+})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <img src ={picture} alt ="relax"/>
+        <h2>KNOW HOW TO SPENT YOUR FREE TIME!</h2>
+        <p className='header'>Maybe you will find something new...</p>
+      </div>
+      <div className='container activity'>
+      <h1>{activities}</h1>
+      <p className='addInfo'>Participants: {participantsActivity}</p>
+      <p className='addInfo type'>Type of activity: {typeActivity}</p>
+      <button onClick={showAdvice}>CLICK</button>
+      </div>
     </div>
   );
 }
